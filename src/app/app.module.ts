ab7374f2745cc2ae199f69b73e115cc434e3ff19
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2023-03-16 17:21:55 +0530

    Added: my first angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloworldComponent } from './Components/helloworld/helloworld.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloworldComponent,
+    ResumeComponent,
   ],
   imports: [
     BrowserModule,
