323a42d3ce245cf8532181a6704ff2dc16dda47c
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2023-03-16 18:43:48 +0530

    Added: Examples of interpolation data binding

---
+++ src/app/Components/Databinding/interpolation/interpolation.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  templateUrl: './interpolation.component.html',
+  styleUrls: ['./interpolation.component.css']
+})
+export class InterpolationComponent {
+
+  dynName: string = "Rutuja Nandiwadekar"
+
+  customMethod() {
+    return "This is " + this.dynName
+  }
+
+  status:boolean=false;
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloworldComponent } from './Components/helloworld/helloworld.component';
-import { ResumeComponent } from './resume/resume.component';
+import { ResumeComponent } from './Components/resume/resume.component';
+import { InterpolationComponent } from './Components/Databinding/interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloworldComponent,
     ResumeComponent,
+    InterpolationComponent,
   ],
   imports: [
     BrowserModule,
